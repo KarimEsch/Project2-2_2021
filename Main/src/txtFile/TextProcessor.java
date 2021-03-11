7b7a294c5350116538c999dc6b411ea53a5b827a
teunhermans <teunhermans@gmail.com>
2021-03-11 19:15:30 +0100

    interaction

---
+++ Main/src/txtFile/TextProcessor.java
@@ -0,0 +1,46 @@
+package txtFile;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TextProcessor {
+    public ArrayList<String> findData(String text){
+        String[] splitSentence = text.split(" ");
+        ArrayList<String> data = new ArrayList<>();
+        for(int i = 0; i < splitSentence.length; i++){
+            if(splitSentence[i].startsWith("<")){
+                data.add(splitSentence[i].substring(1, splitSentence[i].length()-1));
+            }
+        }
+        return data;
+    }
+
+    public String LectureFinder(String input) throws FileNotFoundException {
+
+        String returnString = " ";
+        File planning = new File("C:\\Users\\teunh\\Project2-2_2021\\Main\\skills\\lecture.txt");
+        Scanner reader = new Scanner(planning);
+
+        ArrayList<String> data = this.findData(input);
+
+        while (reader.hasNextLine()){
+            String[] sentence = reader.nextLine().split(" ");
+            if(data.get(0).toLowerCase().equalsIgnoreCase(sentence[0]) && data.get(1).equalsIgnoreCase(sentence[1])){
+                for(int i = 2; i < sentence.length; i++){
+                    returnString = returnString.concat(sentence[i] + " ");
+                }
+            }
+        }
+
+        if (returnString.equals(" ")){
+            returnString = "I could not find the lecture at this moment, try any of these timeslots: 09:00, 11:00, 13:00";
+        }
+
+        return returnString;
+    }
+
+
+}
